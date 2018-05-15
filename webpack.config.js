9cff444ec2d27ab8febadead8eb05832b9a58232
cuiyingchun <3075382844@qq.com>
2018-05-15 09:55:46 +0800

    webpack单独打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -8,7 +9,7 @@ const config = {
     target:'web',
     entry:path.join(__dirname,'src/index.js'),
     output:{
-        filename:'bundle.js',
+        filename:'bundle.[hase:8].js',
         path:path.join(__dirname,'dist')
     },
     module:{
@@ -36,20 +37,20 @@ const config = {
                     'stylus-loader'
                 ]
             },
-            {
-                test:/\.less$/,
-                use:[
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader:'postcss-loader',
-                        options:{
-                            sourceMap:true,
-                        }
-                    },
-                    'less-loader'
-                ]
-            },
+            // {
+            //     test:/\.less$/,
+            //     use:[
+            //         'style-loader',
+            //         'css-loader',
+            //         {
+            //             loader:'postcss-loader',
+            //             options:{
+            //                 sourceMap:true,
+            //             }
+            //         },
+            //         'less-loader'
+            //     ]
+            // },
             {
                 test:/\.sass$/,
                 loader:'style-loader!css-loader!sass-loader'
@@ -80,6 +81,23 @@ const config = {
 }
 
 if(isDev){
+    //开发环境
+    config.module.rules.push(
+        {
+            test:/\.less$/,
+            use:[
+                'style-loader',
+                'css-loader',
+                {
+                    loader:'postcss-loader',
+                    options:{
+                        sourceMap:true,
+                    }
+                },
+                'less-loader'
+            ]
+        }
+    )
     config.devtool = '#cheap-module-eval-source-map'
     config.devServer = {
         port:8000,
@@ -96,6 +114,75 @@ if(isDev){
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
+    )
+}else{
+    //正式环境
+   /* config.entry={
+        app:path.join(__dirname,'src/index.js'),
+        vendor:['vue']
+    }
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push(
+        {
+            test:/\.less$/,
+            use:ExtractPlugin.extract({//抽离css样式插件的 具体应用
+               fallback:'style-loader',//将下面的传出的css代码转化为.css文件
+                use:[
+                    'css-loader',
+                    {
+                        loader:'postcss-loader',
+                        option:{
+                            sourceMap:true
+                        }
+                    },
+                    'less-loader'
+                ]
+            })
+        },
+    )
+    config.plugins.push(
+        //抽离css样式插件的 输出 为 styles.[contentHash:8].css，并为其加上contentHash码
+        new ExtractPlugin('styles.[contentHash:8].css'),       //一个哈希值
+        new webpack.optimize.CommonsChunkPlugin({
+            //CommonsChunkPlugin 插件，是一个可选的用于建立一个独立文件(又称作 chunk)的功能，通过将公共模块拆出来，最终合成的文件能够在最开始的时候加载一次，便存起来到缓存中供后续使用。这个带来速度上的提升
+            //将定义的支持库抽出，减小业务代码.js文件的大小，因为支持库不经常更新，单独抽出可以作为常用文件缓存于浏览器中，减少网络负载
+            name:'vendor'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({//将webpack自身代码抽出为一个单独的.js文件
+            name:'runtime'
+        })
+    )*/
+    config.entry = {
+        app : path.join(__dirname,'src/index.js'),// 重定向入口文件
+        vendor:['vue']// 将支持库例如Vue 源码单独存储
+    }
+    config.output.filename = '[name].[chunkhash:8].js',//设置出口文件的文件名
+    config.module.rules.push(
+        {
+            test: /\.less$/,
+            use: ExtractPlugin.extract({//抽离css样式插件的 具体应用
+                fallback:'style-loader',//将下面的传出的css代码转化为.css文件
+                use:[//.styl文件经过处理后转化为css代码
+                    'css-loader',
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            sourceMap:true,
+                        }
+                    },
+                    'less-loader'
+                ]
+            })
+        }
+    ),
+    config.plugins.push(
+        new ExtractPlugin('styles.[contentHash:8].css'),//抽离css样式插件的 输出 为 styles.[contentHash:8].css，并为其加上contentHash码
+        new webpack.optimize.CommonsChunkPlugin({//CommonsChunkPlugin 插件，是一个可选的用于建立一个独立文件(又称作 chunk)的功能，通过将公共模块拆出来，最终合成的文件能够在最开始的时候加载一次，便存起来到缓存中供后续使用。这个带来速度上的提升
+            name:'vendor'//将支持库抽出，减小业务代码.js文件的大小，因为支持库不经常更新，单独抽出可以作为常用文件缓存于浏览器中，减少网络负载
+        }),
+        new webpack.optimize.CommonsChunkPlugin({//
+            name:'runtime'//将webpack自身代码抽出为一个单独的.js文件
+        })
     )
 }
 
